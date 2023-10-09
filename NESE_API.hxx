bfd2856bd732ef3901520a778dc4b6910380032e
Mikhail Baranin <80689669+Ozsbourn@users.noreply.github.com>
2023-10-09 19:06:24 +0500

    [DEL]: NESE_API wrapper

--- NESE_API.hxx
+++
@@ -1,9 +0,0 @@
-#ifndef NESE_API_HXX_
-#define NESE_API_HXX_
-
-#include <D:/Dev/Libs/SFML-2.5.1/include/SFML/Config.hpp>
-#include <D:/Dev/Libs/SFML-2.5.1/include/SFML/System.hpp>
-#include <D:/Dev/Libs/SFML-2.5.1/include/SFML/Window.hpp>
-#include <D:/Dev/Libs/SFML-2.5.1/include/SFML/Graphics.hpp>
-
-#endif // NESE_API_HXX_
